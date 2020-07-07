d0ca529ae4507f680fca5d014d4c8c28a2532c9a
ewen-edmunds <ewen.edmunds@softwire.com>
2020-07-07 11:15:48 +0100

    Listing Functionality Added

--- SupportBank/Program.cs
+++ SupportBank/Program.cs
@@ -8,36 +8,121 @@ namespace SupportBank
     {
         static void Main(string[] args)
         {
-            
             Console.WriteLine("Welcome to the Support Bank!");
             Console.WriteLine("============================\n");
             
             //todo: Make this work for any user, not just me
-            string[] lines = System.IO.File.ReadAllLines("C:\\Users\\eweedm\\Documents\\Work\\Training\\SupportBank\\Transactions2014.csv");
-            var newLines = lines.Skip(1);
+            string[] allLines = System.IO.File.ReadAllLines("C:\\Users\\eweedm\\Documents\\Work\\Training\\SupportBank\\Transactions2014.csv");
+            var transactionLines = allLines.Skip(1);
             
-            var people = CreateUniqueUsers(newLines);
+            var accountDictionary = CreateUniqueUsers(transactionLines);
+
+            List<Payment> payments = ConvertFromCSV(transactionLines);
             
-            
+            UpdateTransactions(accountDictionary, payments);
+
+            TakeUserInputsForAccounts(accountDictionary);
         }
         
-        static HashSet<string> CreateUniqueUsers(IEnumerable<string> lines){
-            HashSet<string> people = new HashSet<string>();
+        static Dictionary<string, Person> CreateUniqueUsers(IEnumerable<string> lines){
+            HashSet<string> uniqueNames = new HashSet<string>();
+            Dictionary<string, Person> nameAccountDict = new Dictionary<string, Person>();
             
             foreach (string line in lines)
             {
                 var values = line.Split(',');
-                people.Add(values[1]);
-                people.Add(values[2]);
+                uniqueNames.Add(values[1]);
+                uniqueNames.Add(values[2]);
             }
-
+            
             Console.WriteLine("Unique People:");
-            foreach (string person in people)
+            foreach (string person in uniqueNames)
             {
                 Console.WriteLine(person);
+                nameAccountDict.Add(person, new Person(person));
             }
 
-            return people;
+            return nameAccountDict;
+        }
+
+        static List<Payment> ConvertFromCSV(IEnumerable<string> transactionLines)
+        {
+            List<Payment> payments = new List<Payment>();
+            foreach (string line in transactionLines)
+            {
+                var values = line.Split(',');
+                payments.Add(new Payment(values[0], values[1], values[2], values[3], decimal.Parse(values[4])));
+            }
+
+            return payments;
+        }
+
+        static void UpdateTransactions(Dictionary<string, Person> nameAccountDict, IEnumerable<Payment> payments)
+        {
+            foreach (Payment payment in payments)
+            {
+                nameAccountDict[payment.From].AddPayment(payment);
+                nameAccountDict[payment.To].AddPayment(payment);
+            }
+        }
+
+        static void TakeUserInputsForAccounts(Dictionary<string, Person> nameAccountDict)
+        {
+            var isRunning = true;
+            do
+            {
+                Console.WriteLine("\nPlease enter 'List All', or 'List [Account]'");
+                Console.WriteLine("============================================\n>");
+                var userInput = Console.ReadLine();
+                if (userInput == "" || userInput.ToLower() == "quit" || userInput.ToLower() == "q")
+                {
+                    isRunning = false;
+                }
+                else if (userInput == "List All")
+                {
+                    DisplayAllInformation(nameAccountDict); 
+                }
+                else if (userInput.StartsWith("List "))
+                {
+                    try
+                    {
+                        DisplaySpecificUserInformation(userInput.Remove(0, 5), nameAccountDict);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            } while (isRunning);
+        }
+
+        static void DisplayAllInformation(Dictionary<string, Person> nameAccountDict)
+        {
+            Console.WriteLine("\nDisplaying All Accounts:");
+            Console.WriteLine("========================");
+            foreach (var pair in nameAccountDict)
+            {
+                pair.Value.PrintToConsole();
+            }
+        }
+
+        static void DisplaySpecificUserInformation(string user, Dictionary<string, Person> nameAccountDict)
+        {
+            if (nameAccountDict.ContainsKey(user))
+            {
+                Console.WriteLine($"\nDisplaying All Information for user: {user}");
+                nameAccountDict[user].PrintToConsole();
+                
+                Console.WriteLine("\nIndividual Records:");
+                foreach (var payment in nameAccountDict[user].Payments)
+                {
+                    payment.PrintToConsole();
+                }
+            }
+            else
+            {
+                throw new Exception("No user by that name exists.");
+            }
         }
     }
     
@@ -45,22 +130,55 @@ namespace SupportBank
 
     class Person
     {
-        public string name;
-        public float balance;
+        public string Name;
+        public decimal Balance = 0;
+        public List<Payment> Payments = new List<Payment>();
 
         public Person(string newName)
         {
-            this.name = newName;
-            this.balance = 0;
+            this.Name = newName;
+        }
+
+        public void AddPayment(Payment newPayment)
+        {
+            this.Payments.Add(newPayment);
+            
+            if (newPayment.From == this.Name)
+            {
+                this.Balance -= newPayment.Amount;
+            }
+            else
+            {
+                this.Balance += newPayment.Amount;
+            }
+        }
+
+        public void PrintToConsole()
+        {
+            Console.WriteLine($"\nUser: {Name}. \nBalance: {Balance.ToString()}");
         }
     }
 
     class Payment
     {
-        public string date;
-        public string from;
-        public string to;
-        public string narrative;
-        public float amount;
+        public string Date;
+        public string From;
+        public string To;
+        public string Narrative;
+        public decimal Amount;
+
+        public Payment(string date, string from, string to, string narrative, decimal amount)
+        {
+            this.Date = date;
+            this.From = from;
+            this.To = to;
+            this.Narrative = narrative;
+            this.Amount = amount;
+        }
+
+        public void PrintToConsole()
+        {
+            Console.WriteLine($"On {Date}, {From} paid {To} £{Amount.ToString()}, with a narrative of: {Narrative}");
+        }
     }
 }
